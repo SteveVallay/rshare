8e45af148ba0ef2a1e46c098854729baa9d3005c
zhibinw <zhibinwang.q@gmail.com>
2013-08-19 21:19:03 +0800

    add upload function.

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   def new
   end
   def create
-    render text:params[:post].inspect
+    #render text:params[:post].inspect
+    uploaded_io = params[:post][:name]
+    File.open(Rails.root.join('public','uploads',uploaded_io.original_filename),'w') do |file|
+      file.write(uploaded_io.read)
+    end
   end
 end
