d29b837d5e815c0d114be67d52b774d891516dd1
zhibinw <zhibinwang.q@gmail.com>
2013-08-19 21:50:01 +0800

    make file_name unique.

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   def create
     #render text:params[:post].inspect
     uploaded_io = params[:post][:name]
-    File.open(Rails.root.join('public','uploads',uploaded_io.original_filename),'w') do |file|
+    time = Time.now.to_f.to_s() + '_'
+    File.open(Rails.root.join('public','uploads',time + uploaded_io.original_filename),'w') do |file|
       file.write(uploaded_io.read)
     end
   end
