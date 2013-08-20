c90a2ca342297b02fc3cb60a98fbba229f9b7f3f
zhibinw <zhibinwang.q@gmail.com>
2013-08-21 07:29:19 +0800

    save origin name and full name, show them also.

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -4,15 +4,27 @@ class PostsController < ApplicationController
   def create
     #render text:params[:post].inspect
     uploaded_io = params[:post][:name]
-    time = Time.now.to_f.to_s() + '_'
-    File.open(Rails.root.join('public','uploads',time + uploaded_io.original_filename),'w') do |file|
+    full_path = get_upload_path(get_time_f_s() + uploaded_io.original_filename).to_s
+    File.open(full_path,'w') do |file|
       file.write(uploaded_io.read)
     end
     @post = Post.new(params[:post].permit(:title,:text))
+    @post.origin_name = uploaded_io.original_filename
+    @post.real_name = full_path
     @post.save
     redirect_to @post
   end
   def show
     @post = Post.find(params[:id])
   end
+
+private
+
+  def get_upload_path(file_name)
+    return Rails.root.join('public','uploads',file_name)
+  end
+  def get_time_f_s
+    Time.now.to_f.to_s() + '_'
+  end
+
 end
