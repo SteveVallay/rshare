ad48feb3f83fce71a11b81c5464d5f13ea51e835
zhibinw <zhibinwang.q@gmail.com>
2013-08-29 06:44:28 +0800

    add jquery validation.

--- app/app/assets/javascripts/application.js
+++ app/app/assets/javascripts/application.js
@@ -9,8 +9,19 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function () {
+
+$("#new_post").validate({
+debug:true,
+rules:{
+"post[title]":{required:true}
+}
+});
+
+});
+
