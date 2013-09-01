dc7c41768c183441fdf10291dff1094314ac617a
zhibinw <zhibinwang.q@gmail.com>
2013-09-01 23:09:18 +0800

    add delete function.

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -38,6 +38,11 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 
 private
 
