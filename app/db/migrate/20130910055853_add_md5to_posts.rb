f5f16ca98fd955e8ef024874ab23d37f84d4d568
zhibinw <zhibinwang.q@gmail.com>
2013-09-10 14:18:58 +0800

    add md5

---
+++ app/db/migrate/20130910055853_add_md5to_posts.rb
@@ -0,0 +1,4 @@
+class AddMd5toPosts < ActiveRecord::Migration
+  def change
+  end
+end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819140433) do
+ActiveRecord::Schema.define(version: 20130910055853) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
